0c9a70c3929a831eb62dbefc0feaf51da795d44d
adrianoenrico <adrianoenrico1@gmail.com>
2019-10-30 02:01:57 +0000

    added build

---
+++ build/precache-manifest.d980608d020912535fa8bd257f605bf4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1316f035560c3bc9b374ae9e61eb116",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e96b23e997a0ae3b01f",
+    "url": "/static/js/2.27609a8f.chunk.js"
+  },
+  {
+    "revision": "dcc34e8613ad7e7bc663",
+    "url": "/static/js/main.a965dcaf.chunk.js"
+  },
+  {
+    "revision": "bb900868b3bfba6893be",
+    "url": "/static/js/runtime-main.b958e26d.js"
+  }
+]);
